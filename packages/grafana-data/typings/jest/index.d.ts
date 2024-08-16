f3fe770694080d4400580763330d8be2ec3c090c
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-08-16 13:04:47 -0300

    minor changes

--- packages/grafana-data/typings/jest/index.d.ts
+++ packages/grafana-data/typings/jest/index.d.ts
@@ -6,7 +6,7 @@ declare global {
   namespace jest {
     interface Matchers<R, T = {}> {
       toEmitValues<E = ObservableType<T>>(expected: E[]): Promise<CustomMatcherResult>;
-      /**
+      /*
        * Collect all the values emitted by the observables (also errors) and pass them to the expectations functions after
        * the observable ended (or emitted error). If Observable does not complete within OBSERVABLE_TEST_TIMEOUT_IN_MS the
        * test fails.
